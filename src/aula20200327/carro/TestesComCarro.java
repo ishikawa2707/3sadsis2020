fd819e3571f6ab806db27e85a747ff35645ead18
ishikawa2707 <61383299+ishikawa2707@users.noreply.github.com>
2020-03-31 01:43:11 -0300

    Update TestesComCarro.java

--- src/aula20200327/carro/TestesComCarro.java
+++ src/aula20200327/carro/TestesComCarro.java
@@ -10,26 +10,68 @@ class TestesComCarro {
 	void testarConsumo() {
 		Carro palioABC3333 = new Carro();
 		Carro golAGJ1234 = new Carro();
-		
+
 		assertTrue(palioABC3333.isDesligado());
 		assertTrue(golAGJ1234.isDesligado());
-		
+
 		palioABC3333.abastecer(10.00);
 		golAGJ1234.abastecer(25.00);
-		
+
 		palioABC3333.ligar();
 		assertEquals(9.75, palioABC3333.getCombustivelNoTanqueEmLitros());
-		
+
 		golAGJ1234.ligar();
-		assertEquals(24.75, golAGJ1234.getCombustivelNoTanqueEmLitros());	
-		
+		assertEquals(24.75, golAGJ1234.getCombustivelNoTanqueEmLitros());
+
 		palioABC3333.acelerar();
 		palioABC3333.acelerar();
 		palioABC3333.acelerar();
 		assertEquals(9.00, palioABC3333.getCombustivelNoTanqueEmLitros());
-		
+
 		golAGJ1234.acelerar();
 		assertEquals(24.50, golAGJ1234.getCombustivelNoTanqueEmLitros());
+	}
+
+	@Test
+	void abastecerCarro() {
+		Carro palioABC3333 = new Carro();
+		Carro golAGJ1234 = new Carro();
+
+		palioABC3333.abastecer(0.75);
+		golAGJ1234.abastecer(2.00);
+
+		assertEquals(0.75, palioABC3333.getCombustivelNoTanqueEmLitros());
+		assertEquals(2.00, golAGJ1234.getCombustivelNoTanqueEmLitros());
+	}
+
+	void desliguarCarroSemCombustivel() {
+
+		Carro palioABC3333 = new Carro();
+
+		palioABC3333.abastecer(0.75);
+
+		palioABC3333.ligar();
+
+		palioABC3333.acelerar();
+		palioABC3333.acelerar();
+
+		assertEquals(false, palioABC3333.isLigado());
+	}
+
+	@Test
+	void criarCarroComCombustivel() {
+		Carro palioABC3333 = new Carro();
+
+		assertEquals(10.00, palioABC3333.getCombustivelNoTanqueEmLitros());
+
+		palioABC3333.abastecer(0.25);
+
+		palioABC3333.ligar();
+
+		palioABC3333.desligar();
+
+		assertEquals(10.00, palioABC3333.getCombustivelNoTanqueEmLitros());
+
 	}
 
 }
